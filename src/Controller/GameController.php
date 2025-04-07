d0e7c50e87d54bc5ae8000d08baaa855723f0d09
moonye@live.fr <moonye@live.fr>
2025-04-07 16:25:23 +0200

    ADD : ajout visuel temporaire

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\IgdbClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
@@ -15,4 +16,17 @@ class GameController extends AbstractController
         $games = $igdb->searchGames($name);
         return $this->json($games);
     }
+
+    // Début du code temporaire
+    #[Route('/games/search/{query}', name: 'games_search')]
+    public function searchView(string $query, IgdbClient $igdb): Response
+    {
+        $games = $igdb->searchGames($query);
+
+        return $this->render('games/search.html.twig', [
+            'games' => $games,
+            'query' => $query,
+        ]);
+    }
+    // Fin du code temporaire
 }
